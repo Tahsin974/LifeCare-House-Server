4a6a644edc7387c3c1ec2d80a7c91818ae6972d3
Tahsin974 <tahsinislam974@gmail.com>
2025-02-02 18:11:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3lwmdbh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const database = client.db("Lifecare_DB");
+    const doctorCollection = database.collection('doctors');
+    const serviceCollection = database.collection('services');
+    const feedbackCollection = database.collection('feedbacks');
+
+    // Services Collection Related APIs
+    // Get APIs
+    app.get('/services', async(req,res) =>{
+        const result = await serviceCollection.find({}).toArray();
+        res.json(result)
+    })
+    // Doctors Collection Related APIs
+    // Get APIs
+    app.get('/doctors', async(req,res) =>{
+        const result = await doctorCollection.find({}).toArray();
+        res.json(result)
+    })
+    app.get('/doctors/:id', async(req,res) =>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await doctorCollection.findOne(query);
+        res.json(result)
+    })
+
+    
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome To LifeCare House Server");
+});
+
+app.listen(port, () => {
+  console.log("listening to the port", port);
+});
