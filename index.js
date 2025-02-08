2ffa1eb33fc25451c97fa5f31e3d99d82ad9437d
Tahsin974 <tahsinislam974@gmail.com>
2025-02-08 20:36:40 +0600

    available services api creation done

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3lwmdbh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-
 // middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,37 +23,44 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const database = client.db("Lifecare_DB");
-    const doctorCollection = database.collection('doctors');
-    const serviceCollection = database.collection('services');
-    const feedbackCollection = database.collection('feedbacks');
+    const doctorCollection = database.collection("doctors");
+    const serviceCollection = database.collection("services");
+    const feedbackCollection = database.collection("feedbacks");
+    const availableServiceCollection =
+      database.collection("available-services");
 
     // Services Collection Related APIs
     // Get APIs
-    app.get('/services', async(req,res) =>{
-        const result = await serviceCollection.find({}).toArray();
-        res.json(result)
-    })
+    app.get("/services", async (req, res) => {
+      const result = await serviceCollection.find({}).toArray();
+      res.json(result);
+    });
     // Doctors Collection Related APIs
     // Get APIs
-    app.get('/doctors', async(req,res) =>{
-        const result = await doctorCollection.find({}).toArray();
-        res.json(result)
-    })
-    app.get('/doctors/:id', async(req,res) =>{
-        const id = req.params.id;
-        const query = {_id : new ObjectId(id)}
-        const result = await doctorCollection.findOne(query);
-        res.json(result)
-    })
+    app.get("/doctors", async (req, res) => {
+      const result = await doctorCollection.find({}).toArray();
+      res.json(result);
+    });
+    app.get("/doctors/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await doctorCollection.findOne(query);
+      res.json(result);
+    });
 
     // Feedbacks Collection Related APIs
     // Get APIs
-    app.get('/feedbacks', async(req,res) =>{
+    app.get("/feedbacks", async (req, res) => {
       const result = await feedbackCollection.find({}).toArray();
-      res.json(result)
-  })
+      res.json(result);
+    });
 
-    
+    // Available Service Collection Related Apis
+    // Get APIs
+    app.get("/available-services", async (req, res) => {
+      const result = await availableServiceCollection.find({}).toArray();
+      res.json(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
